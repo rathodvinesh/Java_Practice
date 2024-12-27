e4a48d081ca7fda844206312bb2d52c2b6274d4c
rathodvinesh <vineshr4561@gmail.com>
2024-12-27 19:48:06 +0530

    Minor changes

--- src/FileHandling/FileHandling.java
+++ src/FileHandling/FileHandling.java
@@ -1,4 +1,4 @@
-package OOPS.FileHandling;
+package FileHandling;
 
 import java.io.File;
 import java.io.FileWriter;
--- src/JDBC/MySQlConnection.java
+++ src/JDBC/MySQlConnection.java
@@ -1,4 +1,4 @@
-package OOPS.JDBC;
+package JDBC;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
